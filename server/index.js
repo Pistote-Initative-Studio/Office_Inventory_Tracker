0df7c8e39ee1d29a9ed649573b062c9d04d737a2
Peter <pej953@gmail.com>
2025-07-18 19:35:36 -0700

    Add React client and Express server
    Add React client and Express server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Office Inventory Backend Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
